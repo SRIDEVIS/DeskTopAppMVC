17ccff6468f4387b9ee15c93eb6b8c1a74121a36
Vishal Tahiliani <vishal.tahiliani@gmail.com>
2013-01-30 10:12:55 -0500

    Commited first version of desktop MVC

---
+++ MvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class Customer
+    {
+        public string  Name { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetCustomers(int Id)
+        {
+            List<Customer> customers= new List<Customer>(){
+                new Customer(){Name="Vishal"}
+            };
+            return Json(customers);
+        }
+
+    }
+}
---
+++ WindowForm/DesktopExe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RestSharp;
+
+namespace WindowsFormsApplication1
+{
+    public partial class DesktopExe : Form
+    {
+        public DesktopExe()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client =new  RestClient("http://localhost:23033/");
+            var request = new RestRequest("Home/GetCustomers/{id}", Method.POST);
+            request.AddParameter("id", 1);
+            var response = client.Execute(request);
+            MessageBox.Show(response.Content); // raw content as string
+        }
+    }
+}
